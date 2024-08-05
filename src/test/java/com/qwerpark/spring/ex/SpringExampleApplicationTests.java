cc57ff51a30c5996401b4816978b28258f46b50e
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2024-08-05 20:35:21 +0900

    박상훈 - 최초 프로젝트 추가

---
+++ src/test/java/com/qwerpark/spring/ex/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerpark.spring.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
