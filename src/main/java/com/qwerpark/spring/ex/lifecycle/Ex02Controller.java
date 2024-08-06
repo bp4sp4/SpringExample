9f0d19019817d2cf03973451543938d698b1a605
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2024-08-06 21:43:08 +0900

    박상훈 - ex, test 예제 작성 완료 - Thymeleaf  html 연동

---
+++ src/main/java/com/qwerpark/spring/ex/lifecycle/Ex02Controller.java
@@ -0,0 +1,12 @@
+package com.qwerpark.spring.ex.lifecycle;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class Ex02Controller {
+    @RequestMapping("/lifecycle/ex02")
+    public String ex02() {  
+        return "lifecycle/ex02"; 
+    }
+}
